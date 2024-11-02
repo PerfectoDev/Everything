a9da3799ad4a77727861ef99533329d8ecf3b276
Ahmed Sobh <gamerpubg5366@gmail.com>
2024-11-02 23:28:51 +0200

    lts

--- assets/js/fetchingAPIS/addToWishlist.js
+++ assets/js/fetchingAPIS/addToWishlist.js
@@ -1,69 +1,71 @@
 function addToWishlist(productId) {
-  var  useriD = localStorage.getItem('User_id')
-  var  tokeN = localStorage.getItem('token')
+  var userId = localStorage.getItem('User_id');
+  var token = localStorage.getItem('token');
+  var isLoggedIn = localStorage.getItem('isLoggedIn');
+
+  if (isLoggedIn === "true") {
     fetch('https://everyapi.webxy.net/UserOpertion/ProductWishList', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${tokeN}`
-        },
-        body: JSON.stringify({ useriD, productId })
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify({ userId, productId })
     })
     .then(response => response.text())
     .then(data => {
-      
-       
-            const Toast = Swal.mixin({
-                toast: true,
-                position: "bottom-end",
-                showConfirmButton: false,
-                timer: 3000,
-                timerProgressBar: true,
-                didOpen: (toast) => {
-                  toast.onmouseenter = Swal.stopTimer;
-                  toast.onmouseleave = Swal.resumeTimer;
-                }
-              });
-              Toast.fire({
-                icon: "success",
-                title: "تمت إضافة المنتج إلى المفضلة بنجاح!"
-              });
-          
-     })
-}
-
-
-function initializeWishlistButtons(useriD, tokeN, productId) {
-
-    if (!useriD || !tokeN || !productId) {
-        console.error("User ID، token، أو productId غير متوفر.");
-        return;
-    }
-
-
-    const wishlistButtons = document.querySelectorAll(".btn-wishlist");
-    wishlistButtons.forEach(button => {
-        button.addEventListener("click", () => addToWishlist(productId, useriD, tokeN));
+      const Toast = Swal.mixin({
+          toast: true,
+          position: "bottom-end",
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+          }
+        });
+        Toast.fire({
+          icon: "success",
+          title: "تمت إضافة المنتج إلى المفضلة بنجاح!"
+        });
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      Swal.fire({
+          icon: "error",
+          title: "فشل العملية",
+          text: "حدث خطأ أثناء إضافة المنتج."
+      });
     });
+  } else {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: "center",
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+      }
+    });
+    Toast.fire({
+      icon: "error",
+      title: "يجب تسجيل الدخول أولاً!"
+    });
+  }
 }
 
+function initializeWishlistButtons() {
+  const productId = window.productId;
+
+  const wishlistButtons = document.querySelectorAll(".btn-wishlist");
+  wishlistButtons.forEach(button => {
+      button.addEventListener("click", () => addToWishlist(productId));
+  });
+}
 
 document.addEventListener("DOMContentLoaded", () => {
-
-    const userId = window.userId;
-    const token = window.token;
-    const productId = window.productId;
-    
-    initializeWishlistButtons(userId, token, productId);
+  initializeWishlistButtons();
 });
-
-
-
-
-
-
-
-
-
-
-
